529c6dfee1a42b99b6a106a12c5adac372f2f1d3
XAHL19 <xiomyaleja1911@hotmail.com>
2024-10-09 04:25:26 -0500

    6

--- listaPasajeros.cpp
+++ listaPasajeros.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 struct nodo {
-    string nombrePasajero;
+    char nombrePasajero[30];
     int numeroDocumento;
-    char destino;
+    int destino;
     nodo *sig;
 };
 
-nodo *cab = NULL, *aux= NULL, *aux2= NULL;
+nodo *cab, *aux, *aux2;
 void registrarPasajero(){
 
     aux=(struct nodo*) malloc(sizeof(nodo));
@@ -49,11 +50,55 @@ void mostrarListaPasajeros(){
     }
 
 }
+void ordenarListaPorDestino(){
+
+    int temporalDe;
+    int temporalDo;
+    char temporalNo[30];
+
+    aux2 = cab;
+    while(aux2->sig!=NULL){
+        aux = cab;
+        while(aux->sig!=NULL){
+            if(aux->destino > aux->sig->destino){
+                temporalDe = aux->destino;
+                aux->destino = aux->sig->destino;
+                aux->sig->destino = temporalDe;
+                }
+            
+                if(aux->numeroDocumento>aux->sig->numeroDocumento){
+                temporalDo = aux->numeroDocumento;
+                aux->numeroDocumento = aux->sig->numeroDocumento;
+                aux->sig->numeroDocumento = temporalDo;
+
+                for (int i = 0; i < 30; i++) {
+                    temporalNo[i] = aux->nombrePasajero[i];
+                    aux->nombrePasajero[i] = aux->sig->nombrePasajero[i];
+                    aux->sig->nombrePasajero[i] = temporalNo[i];
+                }
+            }
+            
+            aux = aux->sig;
+        }
+        aux2 = aux2->sig;
+    }
+
+
+    cout<<" Lista ordenada por destino y documento: "<<endl;
+    aux = cab;  
+    while (aux != NULL) {  
+        cout<< "Nombre: " <<aux->nombrePasajero <<endl;
+        cout<< "Documento: " <<aux->numeroDocumento <<endl;
+        cout<< "Destino: " <<aux->destino <<endl;
+        cout<< "----------" <<endl;
+        aux = aux->sig;
+    }
+}    
 
 int main(){
     int opcion;
     do {
-        cout<<"Menu"<<endl<<"1. Resgistrar datos de los pasajeros"<<endl<<"2. Mostrar los pasajeros en la lista"<<endl<<"3. Salir del programa"<<endl;
+        cout<<"Menu"<<endl<<"1. Resgistrar datos de los pasajeros"<<endl<<"2. Mostrar los pasajeros en la lista"<<endl<<"3. Lista ordenada por destino y documento"<<endl<<"4. Salir del programa"<<endl;
         cin>>opcion;
         switch (opcion)
         {
@@ -64,8 +109,13 @@ int main(){
             mostrarListaPasajeros();
         break;
         case 3:
+            ordenarListaPorDestino();
+        break;
+        case 4:
             cout << "Saliendo del programa"<<endl;
         break;
-         }
-    }while(opcion!=3);
+        }
+    
+    }while(opcion!=4);
+
 }
